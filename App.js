7f88a1a5cf72a0d2ad6545531334410f026cdc9b
Alberto Ivo Vieira <albertoivo@gmail.com>
2017-12-14 22:33:53 -0200

    fisrt view and statusbar

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, StatusBar } from 'react-native'
 import { TabNavigator } from 'react-navigation'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { black } from './utils/colors'
+import { Constants } from 'expo'
 import ViewDeck from './components/ViewDeck'
 import ViewCard from './components/ViewCard'
+
+function AppStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 const Tabs = TabNavigator({
   Deck: {
@@ -19,6 +29,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <AppStatusBar backgroundColor={black} barStyle="light-content" />
         <Tabs />
       </View>
     )
--- components/ViewDeck.js
+++ components/ViewDeck.js
@@ -5,7 +5,10 @@ export default class ViewDeck extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Deck</Text>
+        <View style={styles.deck}>
+          <Text style={styles.deckName}>This is a deck</Text>
+          <Text style={styles.cardTotalNumber}>3 cards</Text>
+        </View>
       </View>
     )
   }
@@ -15,7 +18,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'stretch'
+  },
+  deck: {
+    borderBottomWidth: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    padding: 20
+  },
+  deckName: {
+    fontWeight: 'bold',
+    padding: 5,
+    fontSize: 30
+  },
+  cardTotalNumber: {
+    opacity: 0.8,
+    padding: 5,
+    fontSize: 20
   }
 })
